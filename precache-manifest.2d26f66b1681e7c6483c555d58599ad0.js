8496ab9584890096204a55b89739ae85c6503a50
rockingatgithub <contactwithsid@gmail.com>
2020-10-01 14:33:40 +0530

    Updates

---
+++ precache-manifest.2d26f66b1681e7c6483c555d58599ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54da6352c485ee8111e3c1a7926dd941",
+    "url": "/reactbook/index.html"
+  },
+  {
+    "revision": "1450619548bf4a3ca193",
+    "url": "/reactbook/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0f0c69e52371ab674fce",
+    "url": "/reactbook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1450619548bf4a3ca193",
+    "url": "/reactbook/static/js/2.bccbd060.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactbook/static/js/2.bccbd060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0c69e52371ab674fce",
+    "url": "/reactbook/static/js/main.934f2140.chunk.js"
+  },
+  {
+    "revision": "a3605eacf8b23ae5dc02",
+    "url": "/reactbook/static/js/runtime-main.398b7292.js"
+  }
+]);
